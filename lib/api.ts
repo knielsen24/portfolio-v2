0b01f76ec820909f536862a963a33d97106e9280
kevinnielsen <knielsen24@gmail.com>
2024-02-03 14:29:19 -0800

    removed env file and api projects route file, causing build time error.

--- lib/api.ts
+++ lib/api.ts
@@ -1,1 +1,13 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL
+// export const API_URL = process.env.NEXT_PUBLIC_API_URL
+
+// import { getAllProjects } from "@/lib/controller";
+// import { NextResponse } from "next/server";
+
+// export async function GET() {
+//   try {
+//     const projects = getAllProjects();
+//     return Response.json({ message: "derelicte", projects }, { status: 200 });
+//   } catch (err) {
+//     return Response.json({ message: "Error", err }, { status: 500 });
+//   }
+// }
