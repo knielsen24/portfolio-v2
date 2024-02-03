25a3d1dffcf226b00df6c5c17cac5b97fc7fd1b9
kevinnielsen <knielsen24@gmail.com>
2024-02-03 07:57:15 -0800

    adjusted api/projects[id] route.ts

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -1,12 +1,6 @@
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 // import { projects } from "@/constants/projects";
 
-// export async function GET() {
-//   const foundProject = projects?.filter(
-//     (proj) => proj.id.toString() === params.id,
-//   );
-
-//   return NextResponse.json({
-//     project: foundProject,
-//   });
-// }
+export async function GET() {
+  return
+}
