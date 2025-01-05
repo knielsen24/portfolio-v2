ea2a13ebed6f47c5098f35a9e73e5890aa5bb717
knielsen24 <knielsen24@gmail.com>
2025-01-04 21:07:11 -0800

    linter: wip

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,19 @@
 module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
   extends: ['next/core-web-vitals', 'prettier'],
   rules: {
     'no-console': ['warn', { allow: ['error'] }],
   },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
+      linterOptions: {
+        reportUnusedDisableDirectives: true,
+      },
+    },
+  ],
 };
